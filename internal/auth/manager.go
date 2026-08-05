61eeafc5bdf0f70f4f3eef4443e6eb299d6f7bb5
Akurosia Kamo <akurosia@akurosia.de>
2026-08-05 12:42:19 +0200

    fix lint again

--- internal/auth/manager.go
+++ internal/auth/manager.go
@@ -151,12 +151,11 @@ func (m *Manager) Authenticate(req *Request) (string, *Error) {
 	if err != nil {
 		return "", &Error{
 			Wrapped:        err,
-			AskCredentials: req.EnableAskCredentials && (
-				req.Credentials.User == "" &&
+			AskCredentials: req.EnableAskCredentials &&
+				(req.Credentials.User == "" &&
 					req.Credentials.Pass == "" &&
 					getStreamKey(req) == "" &&
-					token == ""
-			),
+					token == ""),
 		}
 	}
 
